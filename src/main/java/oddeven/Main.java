7f99b4ebafec11b72e1f2debb1b36fdda14178c4
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-04-16 11:04:19 +0300

    add odd-even sorting

---
+++ src/main/java/oddeven/Main.java
@@ -0,0 +1,40 @@
+package oddeven;
+
+import java.util.Arrays;
+import main.TaskUtil;
+
+/*
+* Odd-even sort based on bubble sort
+* Worst-case performance - O(n^2)
+* Best-case performance -  O(n)
+* Worst-case space complexity -	O(1)
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = TaskUtil.initArray();
+        System.out.println("Odd-even sorting");
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
+        oddevenSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+
+    private static void oddevenSort(int[] arr) {
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < arr.length - 1; i += 2) {
+                if (arr[i] > arr[i + 1]) {
+                    TaskUtil.swap(arr, i,i + 1);
+                    sorted = false;
+                }
+            }
+            for (int i = 1; i < arr.length; i += 2) {
+                if (arr[i] > arr[i + 1]) {
+                    TaskUtil.swap(arr, i,i + 1);
+                    sorted = false;
+                }
+            }
+        }
+    }
+}
